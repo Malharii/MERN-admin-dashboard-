5dc47f86693911cbafc4d57892d63091091ddbb9
Malharii <mrmalhari96@gmail.com>
2024-10-27 22:22:46 +0530

    solved theme errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,18 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "./theme";
+
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
       <h1>Hello malhar welocome to your admin panel</h1>
     </>
   );
